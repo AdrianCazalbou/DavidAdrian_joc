ba70f1681a3bcdb5fa5a302e12233f96c0190dce
Partal99 <79510055+AdrianCazalbou@users.noreply.github.com>
2021-05-11 12:25:13 +0200

    plataforma 50/50

--- desenvolupament.gd
+++ desenvolupament.gd
@@ -6,9 +6,12 @@ export var escena_plataforma : PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var nova_plataforma = escena_plataforma.instance()
-	nova_plataforma.position = Vector2(220, 580)
+	nova_plataforma.position = Vector2(RandomNumberGenerator(100, 700), 580)
 	add_child(nova_plataforma)
+	var nova_plataforma2 = escena_plataforma.instance()
+	nova_plataforma.position = Vector2(RandomNumberGenerator(100, 700), )
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+
 	$fondo.position.y = $dorimon.position.y
--- dorimon.gd
+++ dorimon.gd
@@ -8,7 +8,7 @@ var velocitat_max = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	connect("body_entered", Global.dorimon, "_on_Area2D_body_entered")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
